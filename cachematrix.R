df9b9b452667f56e9fa71b3bed8eaef19c31fbd1
roxanne990 <137737025+roxanne990@users.noreply.github.com>
2023-07-12 16:50:09 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,17 +6,17 @@
 ## inverted matrix elements.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function(y) {
-    x <<- y
-    inv <<- NULL
-  }
-  get <- function () x
-  setinverse <- function(inverse) inv <<- inverse
-  getinverse <- function() inv
-  list (set = set, get = get,
-        setinverse = setinverse, 
-        getinverse = getinverse)
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function () x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list (set = set, get = get,
+              setinverse = setinverse, 
+              getinverse = getinverse)
 }
 
 
@@ -24,13 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## by the "makeCacheMatrix" above. I changed mean -> inverse and m -> inv from 
 ## the "cachemean" example.
 cacheSolve <- function(x, ...) {
-  inv <- x$getinverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get()
-  inv <- solve(data, ...)
-  x$setinverse(inv)
-  inv
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
